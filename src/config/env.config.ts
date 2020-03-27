bfb17e2b0c17f0aeec218d538543294950450652
christelle thiry <christelle.thiry@hotmail.fr>
2020-03-27 07:43:57 +0100

    Add .gitignore file

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,15 +2,15 @@ import * as dotEnv from 'dotenv';
 dotEnv.config();
 
 export const ENV = {
-    PORT: process.env.PORT || '3000',
+    PORT: process.env.PORT,
 
-    DB_HOST: process.env.DB_HOST || '127.0.0.1',
-    DB_PORT: process.env.DB_PORT || '3306',
-    DB_NAME: process.env.DB_NAME || 'dbName',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
-    DB_DIALECT: process.env.DB_DIALECT || 'mysql',
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DIALECT: process.env.DB_DIALECT,
 
-    JWT_ENCRYPTION: process.env.JWT_ENCRYPTION || 'secureKey',
-    JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1y',
+    JWT_ENCRYPTION: process.env.JWT_ENCRYPTION,
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION,
 };
